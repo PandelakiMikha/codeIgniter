5f45b60a54c186d12a0f4d24e9b670cd49023dd4
Abelardjohn <abelardpangalila@gmail.com>
2022-08-01 11:22:39 +0800

    fix conflict

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,6 +6,7 @@
 
 <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
 <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/js/userjs.js"></script>
 
 <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
@@ -16,13 +17,37 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 <!-- select bertingkat -->
-<script>
+<script type="text/javascript">
+    var table;
     $(document).ready(function() {
         $("#perangkat_daerah").hide();
 
-        loadDaerah();
-
-        tabels();
+        //data tables
+        table = $('#example').DataTable({
+            "processing": true,
+            "serverSide": true,
+            "order": [],
+            "ajax": {
+                "url": "<?= base_url('ServerSideTables/getData'); ?>",
+                "type": "POST",
+                "data": function(data) {
+                    // var perda;
+                    data.daerah = $("#daerah").val();
+                    // perda = data.perangkat_daerah;
+                    // console.log('perda', perda);
+                },
+            },
+            "columnDefs": [{
+                "target": [-1],
+                "orderable": false,
+                "searchable": false,
+                "render": function(data, type, row) {
+                    var btn = '<div class="cuss"><div><button type="button" class="btn btn-warning" data-bs-trigger="focus" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover"><i class="bi bi-eye"></i>Lihat</button></div><div class="middle"><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-check-circle"></i>Disposisi</button></div><div><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-file-earmark-text"></i>Detail</button></div></div>';
+                    return btn;
+                }
+            }]
+        });
+        loadPerangkatDaerah();
     });
     $(document).ready(function() {
         $("#perangkat_daerah2").show();
@@ -33,13 +58,8 @@
     });
 
 
-    function loadDaerah() {
-        $("#perangkat_daerah").change(function() {
-            var getPerangkatDaerah = $("#perangkat_daerah").val();
-            // console.log(getPerangkatDaerah);
-            tabels();
-        });
-
+    function loadPerangkatDaerah() {
+        // for get data for filter
         $("#daerah").change(function() {
             var getDaerah = $("#daerah").val();
             $.ajax({
@@ -50,7 +70,7 @@
                     daerah: getDaerah,
                 },
                 success: function(data) {
-                    console.log(data);
+                    console.log('data', data);
 
                     var html = "";
                     var i;
@@ -64,7 +84,7 @@
             });
 
         });
-    };
+    }
 
     //function untuk krim surat.....
 
@@ -152,6 +172,13 @@
                     return btn;
                 }
             }]
+        })
+        $('#btn-filter').click(function() { //button filter event click
+            table.ajax.reload(); //just reload table
+        });
+        $('#btn-reset').click(function() { //button reset event click
+            $('#form-filter')[0].reset();
+            table.ajax.reload(); //just reload table
         });
     };
 </script>
